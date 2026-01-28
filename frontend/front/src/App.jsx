f0f001d5a087afe8225dece9350210b176166202
mehar.patel <mehar.patel@srkay.com>
2026-01-28 15:55:56 +0530

    firebase

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -1,14 +1,14 @@
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import Signin from "./pages/Signin";
+import Signup from "./Pages/Signup";
 
 export default function App() {
   return (
     <div>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
     </div>
   );
--- frontend/front/src/Pages/Signup.jsx
+++ frontend/front/src/Pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Signin() {
+export default function Signup() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-pink-600 to-purple-700">
       <div className="w-full max-w-md bg-white/10 backdrop-blur-xl p-8 rounded-2xl shadow-xl border border-white/20">
