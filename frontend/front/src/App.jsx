ae18140325e68d800cc1ed6541160300ac7ec86b
mehar.patel <mehar.patel@srkay.com>
2026-01-28 15:00:48 +0530

    login

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signin from "./pages/Signin";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signin" element={<Signin />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+    </div>
   );
 }
